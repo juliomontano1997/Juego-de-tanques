2f5c379fa6036e1695b98ff01da028eb7711fd78
Julio Adan <juliomontano008@gmail.com>
2017-11-11 11:36:53 -0600

    canvas

--- script.js
+++ script.js
@@ -1,26 +1,197 @@
-function figura()
+function Objetos_animados(x,y,dir_imagen)
 {
-    this.pos_x;
-    this.pos_y;
+    this.imagen = new Image();
+    this.imagen.src = dir_imagen;
+    this.pos_x= x;
+    this.pos_y= y;
 
-    this.mover = function () {
-
+    this.mover = function (direccion)
+    {
+        console.log("moviendome"+direccion);
+        if(direccion==="derecha")
+        {
+            this.pos_x++;
+        }
+        else if(direccion==="izquierda")
+        {
+            this.pos_x--;
+        }
+        else if(direccion==="arriba")
+        {
+            this.pos_y--;
+        }
+        else
+        {
+            this.pos_y++;
+        }
     };
 }
 
-function tanque()
+window.onload = function() {
+    document.onkeyup = muestraInformacion;
+    document.onkeypress = muestraInformacion;
+    document.onkeydown = muestraInformacion;
+};
+
+function muestraInformacion(elEvento)
 {
+    var evento = window.event || elEvento;
+    var mensaje = "Tipo de evento: " + evento.type + "<br>" +
+        "Propiedad keyCode: " + evento.keyCode + "<br>" +
+        "Propiedad charCode: " + evento.charCode + "<br>" +
+        "Carácter pulsado: " + String.fromCharCode(evento.charCode);
+}
 
+/*
+
+
+
+
+var prueba = new Objetos_animados(0, 0, 'imagenes/enemigo2.png');
+
+
+function mocer(elEvento) {
+    var evento = window.event || elEvento;
+
+    if (evento.charCode === 37) {
+        prueba.mover("izquierda");
+    }
+    else if (evento.charCode === 38) {
+        prueba.mover("arriba");
+    }
+    else if (evento.charCode === 39) {
+        prueba.mover("derecha");
+    }
+    else if (evento.charCode === 40) {
+        prueba.mover("abajo");
+    }
+}
+
+
+var bandera = true;
+function inicio()
+{
 }
 
 
 
-function enemigos() {
+function fin()
+{
+    bandera = false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ----------------   El siguiente codigo son solo pruebas -------------------
+
+
+
+
+
+function anim() {
+    ctx.clearRect(0, 0, 600, 200);
+    ctx.drawImage(fondo,0, 0);
+    ctx.drawImage(img, pos_x, 0);
+    pos_x += 1;
+    setTimeout(anim, 25);
+}
+
+
+
+
+
+function dibujarMapa()
+{
+    var canvas = document.getElementById('lienzo');
+    var contexto = canvas.getContext('2d');
+
+    // imagen
+    var imagen = new Image();
+    imagen.onload = function(){
+        contexto.drawImage(imagen,0,0);  // imagen completa en la posición (0,0)
+        //contexto.drawImage(imagen,285,0, 150,107); // escalado
+        //contexto.drawImage(imagen,130,85,80,60,285,205,150,107); // escalado de una porción
+        //contexto.strokeStyle = 'yellow';
+        //contexto.strokeRect (130,85,80,60); // rectángulo amarillo
+    };
+    imagen.src = 'imagenes/fondo.jpg';
+}
+
+var cnv, ctx, pos_x = 0, img, fondo;
+
+
+
+function play1()
+{
+    img = new Image();
+    fondo = new Image();
+    img.src = 'imagenes/enemigo2.png';
+    fondo.src = "imagenes/fondo.jpg";
+
+    cnv = document.getElementById('lienzo');
+    ctx = cnv.getContext('2d');
+    // iniciar la animación
+    anim();
+}
+
+
+// Son las figuras: (tanques, balas)
+function Figura_con_movimiento(x,y)
+{
+    this.pos_x= x;
+    this.pos_y= y;
+    this.mover = function (direccion)
+    {
+        if(direccion==="derecha")
+        {
+            this.pos_x++;
+        }
+        else if(direccion==="izquierda")
+        {
+            this.pos_x--;
+        }
+        else if(direccion==="arriba")
+        {
+            this.pos_y--;
+        }
+        else
+        {
+            this.pos_y++;
+        }
+    };
+}
+
+
+function Tanque()
+{
+    this.disparar = function ()
+    {
+        // aqui lanza un disparo
+    }
+}
+
+function Enemigo()
+{
+    // se pueden ubicar el tanque aliado ya sea por un metodo o por una variable global.
 
 }
 
-function amigo() {
-
+function Amigo()
+{
 }
 
 function proyectil()
@@ -30,8 +201,23 @@ function proyectil()
 
 
 
+// Son las figuras que no tienen movimientos (paredes y objetivos)
+function Figura_sin_movimiento(x,y)
+{
+    this.pos_x=x;
+    this.pos_y=y;
+}
 
-/*
+function Muro()
+{
+}
+
+function Pared()
+{
+}
+
+
+
 
 
 
